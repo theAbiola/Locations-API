eb5797d73c950cdd90c98bb4afe90fbce59b46c7
Abdulraheem Habib Abiola <iamhabibabiola@gmail.com>
2025-02-14 11:48:46 +0100

    change mapsURL to mapURL

--- index.js
+++ index.js
@@ -194,7 +194,7 @@ var locations = [
     id: 1,
     locationName: "The Junkyard grills, Central Area, Abuja.",
     locationType: "Outdoor/indoor",
-    mapsURL: "https://www.google.com/maps/place/The+Junkyard+Grills/@9.0656501,7.4871531,17z/data=!3m1!4b1!4m6!3m5!1s0x104e0b876430e6b3:0xaa7826a359e8ae70!8m2!3d9.0656501!4d7.489728!16s%2Fg%2F11g232csty?entry=ttu&g_ep=EgoyMDI1MDIwNS4xIKXMDSoASAFQAw%3D%3D",
+    mapURL: "https://www.google.com/maps/place/The+Junkyard+Grills/@9.0656501,7.4871531,17z/data=!3m1!4b1!4m6!3m5!1s0x104e0b876430e6b3:0xaa7826a359e8ae70!8m2!3d9.0656501!4d7.489728!16s%2Fg%2F11g232csty?entry=ttu&g_ep=EgoyMDI1MDIwNS4xIKXMDSoASAFQAw%3D%3D",
     affordability: "3.5/5",
     rating: "4/5",
   },
@@ -202,7 +202,7 @@ var locations = [
     id: 2,
     locationName: "The Pasha Restaurant, Maitama, Abuja.",
     locationType: "Indoor",
-    mapsURL: "https://www.google.com/maps/place/The+Pasha/@9.0754602,7.4961043,17z/data=!4m9!3m8!1s0x104e0a4940cc7e1f:0x9d201249e6ad70ab!5m2!4m1!1i2!8m2!3d9.0754602!4d7.4986792!16s%2Fg%2F1v1m909v?entry=ttu&g_ep=EgoyMDI1MDIwNS4xIKXMDSoASAFQAw%3D%3D",
+    mapURL: "https://www.google.com/maps/place/The+Pasha/@9.0754602,7.4961043,17z/data=!4m9!3m8!1s0x104e0a4940cc7e1f:0x9d201249e6ad70ab!5m2!4m1!1i2!8m2!3d9.0754602!4d7.4986792!16s%2Fg%2F1v1m909v?entry=ttu&g_ep=EgoyMDI1MDIwNS4xIKXMDSoASAFQAw%3D%3D",
     affordability: "3/5",
     rating: "4.5/5",
   },
@@ -210,7 +210,7 @@ var locations = [
     id: 3,
     locationName: "The Milkshop and Breakfast company, Maitama, Abuja",
     locationType: "Indoor/café",
-    mapsURL: "https://www.google.com/maps/place/The+Breakfast+Company/@9.0873612,7.4936229,17z/data=!4m10!1m2!2m1!1sThe+Milkshop+and+Breakfast+company,+Maitama,+Abuja!3m6!1s0x104e0af9aaaaaaab:0xeaf2216f682d03aa!8m2!3d9.0873612!4d7.4983865!15sCjJUaGUgTWlsa3Nob3AgYW5kIEJyZWFrZmFzdCBjb21wYW55LCBNYWl0YW1hLCBBYnVqYVoyIjB0aGUgbWlsa3Nob3AgYW5kIGJyZWFrZmFzdCBjb21wYW55IG1haXRhbWEgYWJ1amGSARRicmVha2Zhc3RfcmVzdGF1cmFudOABAA!16s%2Fg%2F11knncwhs6?entry=ttu&g_ep=EgoyMDI1MDIwNS4xIKXMDSoASAFQAw%3D%3D",
+    mapURL: "https://www.google.com/maps/place/The+Breakfast+Company/@9.0873612,7.4936229,17z/data=!4m10!1m2!2m1!1sThe+Milkshop+and+Breakfast+company,+Maitama,+Abuja!3m6!1s0x104e0af9aaaaaaab:0xeaf2216f682d03aa!8m2!3d9.0873612!4d7.4983865!15sCjJUaGUgTWlsa3Nob3AgYW5kIEJyZWFrZmFzdCBjb21wYW55LCBNYWl0YW1hLCBBYnVqYVoyIjB0aGUgbWlsa3Nob3AgYW5kIGJyZWFrZmFzdCBjb21wYW55IG1haXRhbWEgYWJ1amGSARRicmVha2Zhc3RfcmVzdGF1cmFudOABAA!16s%2Fg%2F11knncwhs6?entry=ttu&g_ep=EgoyMDI1MDIwNS4xIKXMDSoASAFQAw%3D%3D",
     affordability: "4.5/5",
     rating: "3.5/5"
   },
@@ -218,7 +218,7 @@ var locations = [
     id: 4,
     locationName: "Truck Central, Central Business District, Abuja",
     locationType: "Outdoor",
-    mapsURL: "https://www.google.com/maps/place/Truck+Central/@9.0554115,7.4729394,17z/data=!3m1!4b1!4m6!3m5!1s0x104e0b26dd10aed1:0xc6882c6bc9e0d958!8m2!3d9.0554115!4d7.4755143!16s%2Fg%2F11txgxfls2?entry=ttu&g_ep=EgoyMDI1MDIwNS4xIKXMDSoASAFQAw%3D%3D",
+    mapURL: "https://www.google.com/maps/place/Truck+Central/@9.0554115,7.4729394,17z/data=!3m1!4b1!4m6!3m5!1s0x104e0b26dd10aed1:0xc6882c6bc9e0d958!8m2!3d9.0554115!4d7.4755143!16s%2Fg%2F11txgxfls2?entry=ttu&g_ep=EgoyMDI1MDIwNS4xIKXMDSoASAFQAw%3D%3D",
     affordability: "3/5",
     rating: "4.5/5"
   },
@@ -226,7 +226,7 @@ var locations = [
     id: 5,
     locationName: "Jabi Boat Club, Jabi, Abuja",
     locationType: "Indoor/Outdoor",
-    mapsURL: "https://www.google.com/maps/place/The+Boat+Club/@9.0704918,7.4098442,16.4z/data=!4m6!3m5!1s0x104e75a6db04f073:0xea51a3b8bd7f150f!8m2!3d9.0697441!4d7.4146742!16s%2Fg%2F11fkdh7_5_?entry=ttu&g_ep=EgoyMDI1MDIwOS4wIKXMDSoASAFQAw%3D%3D",
+    mapURL: "https://www.google.com/maps/place/The+Boat+Club/@9.0704918,7.4098442,16.4z/data=!4m6!3m5!1s0x104e75a6db04f073:0xea51a3b8bd7f150f!8m2!3d9.0697441!4d7.4146742!16s%2Fg%2F11fkdh7_5_?entry=ttu&g_ep=EgoyMDI1MDIwOS4wIKXMDSoASAFQAw%3D%3D",
     affordability: "3.2/5",
     rating: "4.7/5"
   },
@@ -234,7 +234,7 @@ var locations = [
     id: 6,
     locationName: "Otega Restaurant 24/7, Wuse 2, Abuja",
     locationType: "Indoor/Outdoor",
-    mapsURL: "https://www.google.com/maps?sca_esv=3a30a683573ba2c0&output=search&q=otega+restaurant+abuja&source=lnms&fbs=ABzOT_CWdhQLP1FcmU5B0fn3xuWpA-dk4wpBWOGsoR7DG5zJBv10Kbgy3ptSBM6mMfaz8zA4dsQnj5xEL8e8yrf6YrQm2qFvxjgbs3veW7IUFM9xHvD0ItLpuFy20BiHDRg8UkwegrJm9FjVXkCMs8DR7asGIoaBVTYmq2yX0eKyl6pRp3mZqNQ4WtReciBlhE1JgAnshoNQ&entry=mc&ved=1t:200715&ictx=111",
+    mapURL: "https://www.google.com/maps?sca_esv=3a30a683573ba2c0&output=search&q=otega+restaurant+abuja&source=lnms&fbs=ABzOT_CWdhQLP1FcmU5B0fn3xuWpA-dk4wpBWOGsoR7DG5zJBv10Kbgy3ptSBM6mMfaz8zA4dsQnj5xEL8e8yrf6YrQm2qFvxjgbs3veW7IUFM9xHvD0ItLpuFy20BiHDRg8UkwegrJm9FjVXkCMs8DR7asGIoaBVTYmq2yX0eKyl6pRp3mZqNQ4WtReciBlhE1JgAnshoNQ&entry=mc&ved=1t:200715&ictx=111",
     affordability: "4/5",
     rating: "4.7/5"
   },
@@ -242,7 +242,7 @@ var locations = [
     id: 7,
     locationName: "Kebabs & Kurries, Wuse 2, Abuja",
     locationType: "Indoor",
-    mapsURL: "https://www.google.com/maps/place/Kebabs+%26+Kurries+Restaurant/@9.0822106,7.4668932,17z/data=!3m1!4b1!4m6!3m5!1s0x104e0b5a0923a7b9:0x15af6c7556150075!8m2!3d9.0822106!4d7.4694681!16s%2Fg%2F11sbhn6_nc?entry=ttu&g_ep=EgoyMDI1MDIxMi4wIKXMDSoASAFQAw%3D%3D",
+    mapURL: "https://www.google.com/maps/place/Kebabs+%26+Kurries+Restaurant/@9.0822106,7.4668932,17z/data=!3m1!4b1!4m6!3m5!1s0x104e0b5a0923a7b9:0x15af6c7556150075!8m2!3d9.0822106!4d7.4694681!16s%2Fg%2F11sbhn6_nc?entry=ttu&g_ep=EgoyMDI1MDIxMi4wIKXMDSoASAFQAw%3D%3D",
     affordability: "4/5",
     rating: "4.2/5"
   },
