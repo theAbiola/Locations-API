24f58dbf7591a5b3195524edb80e9ec5c0ec1dfd
Abdulraheem Habib Abiola <iamhabibabiola@gmail.com>
2025-03-03 14:18:35 +0100

    change bodyparser middleware instance to express

--- server.js
+++ server.js
@@ -2,13 +2,16 @@ import express from "express";
 import bodyParser from "body-parser";
 import env from "dotenv";
 import locationRoutes from "./routes/location.routes.js"
+import mongoose from "mongoose";
+
+
 
 export const app = express();
 env.config();
 const port = process.env.API_PORT;
 
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(bodyParser.json()); //Important to parse incoming JSON requests
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json()); //Important to parse incoming JSON requests
 
 app.use("/locations", locationRoutes);
 
