f9bd56088a0fdd7d41dea24df6f2dc665f2c3381
Abdulraheem Habib Abiola <iamhabibabiola@gmail.com>
2025-03-03 14:31:03 +0100

    connect to a mongoDB database

--- server.js
+++ server.js
@@ -1,10 +1,9 @@
 import express from "express";
-import bodyParser from "body-parser";
 import env from "dotenv";
 import locationRoutes from "./routes/location.routes.js"
 import mongoose from "mongoose";
 
-
+const uri = "mongodb+srv://iamabiola:MOuxfXnva7FtPpaW@apibackenddb.vfwfa.mongodb.net/?retryWrites=true&w=majority&appName=ApiBackendDB"
 
 export const app = express();
 env.config();
@@ -12,6 +11,14 @@ const port = process.env.API_PORT;
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json()); //Important to parse incoming JSON requests
+
+mongoose.connect(uri)
+  .then(() => {
+    console.log("Successfully connected to the MongoDB Database");
+  })
+  .catch((error) => {
+    console.error(`${error}: Connection Failed!`);
+  });
 
 app.use("/locations", locationRoutes);
 
